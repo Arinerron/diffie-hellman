624f4e87b1cd22a5b9d33a5852f57805ec465cdb
Arinerron <arinesau@gmail.com>
2017-07-09 23:28:14 -0700

    Add files

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.math.BigInteger;
+
+public class Main {
+    public int p, g, a, key = 0;
+
+    public static void main(String[] args) {
+        // Diffie-Hellman key exchange
+        Main a = new Main(23, 6, 15);
+        Main b = new Main(23, 6, 6);
+        a.key = a.priv(b.pub());
+        b.key = b.priv(a.pub());
+
+        System.out.println(a.key + " and " + b.key);
+    }
+
+    public Main(int p, int g, int a) { // a is secret integer
+        this.p = p;
+        this.g = g;
+        this.a = a;
+    }
+
+    public int priv(int k) {
+        return new BigInteger(k + "").pow(a).mod(new BigInteger(p + "")).intValue();
+    }
+
+    public int pub() {
+        return new BigInteger(g + "").pow(a).mod(new BigInteger(p + "")).intValue();
+    }
+}
